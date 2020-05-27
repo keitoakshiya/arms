0f32f0eee6cd0c4c3d3efaaea178c4fe923298fc
unknown <kellyeleola@gmail.com>
2020-05-27 22:26:13 +0800

    may 27[login.php line 37] n

--- application/views/login.php
+++ application/views/login.php
@@ -34,6 +34,7 @@
               <h1>Login Form</h1>
               <h1>Message here: git pulled file from may 25</h1>
               <h2>Message here: Testing!!</h2>
+              <h2>Message here: trying to push using nam-git</h2>
               <div>
                 <input type="text" class="form-control" placeholder="Username" name="username" required="" />
               </div>
