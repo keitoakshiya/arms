3f63d8716fca47ce60e0b474f7596e2066c36848
unknown <kellyeleola@gmail.com>
2020-06-18 00:54:57 +0800

    june 18[accounts_receivable: patient_type]n

--- application/views/accounts_receivable.php
+++ application/views/accounts_receivable.php
@@ -13,10 +13,27 @@
 				foreach ($result as $key => $row) {
 					echo "<tr>";
 						echo "<td>".$row->first_name." ".$row->middle_name." ".$row->last_name."</td>";
-						echo "<td>".$row->patient_type."</td>";
-						echo "<td>&#8369; ".$row->hospital_bill."</td>"; /*DON"T FORGET TO ADD PESO SIGN ON OTHER PAGES*/
-						echo "<td>&#8369; ".$row->professional_bill."</td>";
-						echo "<td>&#8369; ".($row->hospital_bill+$row->professional_bill)."</td>";
+/*						echo "<td>".$row->patient_type."</td>";*/
+
+						if ($row->patient_type==1) {
+							echo "<td>Inpatient</td>";
+						}
+
+						else if ($row->patient_type==2) {
+							echo "<td>Outpatient</td>";
+						}
+
+						else if ($row->patient_type==3) {
+							echo "<td>Emergency</td>";
+						}
+
+						else{
+							echo "<td></td>";
+						}
+
+						echo "<td>&#8369;".$row->hospital_bill."</td>"; /*DON"T FORGET TO ADD PESO SIGN ON OTHER PAGES*/
+						echo "<td>&#8369;".$row->professional_bill."</td>";
+						echo "<td>&#8369;".($row->hospital_bill+$row->professional_bill)."</td>";
 						echo "<td>".$row->date."</td>";
 					echo "</tr>";
 				}
