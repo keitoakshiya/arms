94a48de3598c1ffc40a74744d48ca274131cf8f1
unknown <kellyeleola@gmail.com>
2020-06-21 21:21:11 +0800

    june 21[add company, official receipt page]n

--- application/views/add_company.php
+++ application/views/add_company.php
@@ -1,15 +1,18 @@
-
-
+	        <div class="right_col" style="">
+	         <div class="row">
 
 				<!-- page content -->
-
-		            
-		              
-		                
-		                  <ul>
+		            <div class="col-md-4 col-sm-4 ">
+		              <div class="x_panel">
+		                <div class="x_title">
+		                  <ul class="nav navbar-right panel_toolbox">
 		                    <li style="padding-left: 51px;"><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
 		                    </li>
 		                  </ul>
+		                  <div class="clearfix"></div>
+		                </div>
+			                <div class="x_content">
+		                  <div class="dashboard-widget-content">
 		                  		Guarantor:
 			                  <form method="post" action="/arms/main/insert_company">
 			                   	  <select name="type" class="form-control" required>
@@ -18,10 +21,14 @@
 								    <option value="2">Government</option>
 								    <option value="3">Corporate</option>
 								  </select>
-								
-			                   	<input type="submit" value="Add Company" class=" form form-control btn btn-success submit-btn"> 		
+								Company Name: <input type="text" class="form-control" name="name">
+			                   	<input type="submit" value="Add Company" class=" form form-control btn btn-success submit-btn">
 			                  </form>
-		                  
-		                
-		            
+		                  </div>
+		                </div>
+		              </div>
+		            </div>
 		        <!-- /page content -->
+		        
+	          </div>
+	          </div> 
